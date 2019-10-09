7f18b7a8a7221226aa1342180de4b11dfa599b32
teststud@storans.school.nz <teststud@storans.school.nz>
2019-10-10 11:13:50 +1300

    Basic program to check if person is eligible for superannuation.

--- super.py
+++ super.py
@@ -1,9 +1,14 @@
+# Check if person is eligible for superannuation
+
+# Age is a constant because it's not changed in the program
 SUPER_AGE = 65
 
+# Loop to allow testing of different values
 keep_going = ""
 while keep_going == "":
     age = int(input("How old are you?"))
 
+    # Must be greater than OR equal to catch 65 year olds
     if age >= SUPER_AGE:
         print("You are eligible for Govt super.")
     else:
