e7d0768e0de8585c685b0113dec8d785ef03e190
teststud@storans.school.nz <teststud@storans.school.nz>
2019-10-09 21:38:46 +1300

    This is just a test that my dictionary works and that I can access it. I'm also going to test using a 2D list for this ...

---
+++ test_dict.py
@@ -0,0 +1,11 @@
+# Make dictionary for food and points
+
+# Test I can access the dictionary by key
+
+FOOD = {"beetroot": 3, "banana": 4, "pineapple": 1}
+
+print(FOOD)
+
+print(FOOD.get("beetroot"))
+
+test = input("Enter test here")
